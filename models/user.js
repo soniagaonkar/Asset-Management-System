bf7497a6ae95a7fe16e94b8dca478b38841aab28
rashmi-sakhalkar <rashmi_sakhalkar@persistent.com>
2016-06-28 12:08:25 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,9 +1,11 @@
 var mongoose = require('./../config/db');
 
 var UserSchema = new mongoose.Schema({
-    sl_ID: { type: String },
-    isLoggedin:  Boolean,
+    loginId: { type: String },
+	password: { type: String },
+	role: { type: String },
     lastLogin: { type: Date, default: Date.now },
+	createdDate: { type: Date, default: Date.now },
 });
 
 var loggedUser = mongoose.model('user', UserSchema);
